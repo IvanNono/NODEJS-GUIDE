33c5e3a774e02cfd0e4bd20af5457890d23bd5e2
IvanNono <50101557+IvanNono@users.noreply.github.com>
2021-09-08 17:38:29 +0200

    Improved Development Workflow and Debugging

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const http = require('http');
 
 const routes = require('./routes');
 
-const server1 = http.createServer(routes.handler);
-const server = http.createServer(routes.someText);
+console.log(routes.someText);
+
+const server = http.createServer(routes.handler);
+
 
 server.listen(3000);
